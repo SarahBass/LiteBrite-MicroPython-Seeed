f806624912e4f819fbd606d35858f12d0470d822
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-09-10 14:47:29 -0700

    Create MorseCode.py

---
+++ MorseCode.py
@@ -0,0 +1,110 @@
+from ws2812 import WS2812
+import utime
+import machine
+power = machine.Pin(11,machine.Pin.OUT)
+power.value(1)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+ORANGE = (200, 150, 0)
+YELLOWGREEN = (100, 255, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+VIOLET = (255, 0, 255)
+PURPLE = (180, 0, 255)
+MAGENTA = (200, 0, 150)
+WHITE = (255, 255, 255)
+COLORS = (RED, YELLOW, ORANGE, YELLOWGREEN, GREEN, CYAN, BLUE, PURPLE, MAGENTA, WHITE)
+led = WS2812(12,1)#WS2812(pin_num,led_count)
+
+while True:
+    #dah
+    if "-"
+    led.pixels_fill(BLACK)
+    led.pixels_show()
+    utime.sleep(0.2)
+    led.pixels_fill(WHITE)
+    led.pixels_show()
+    utime.sleep(0.5)   
+    led.pixels_fill(BLACK)
+    led.pixels_show()
+    utime.sleep(0.2)
+    
+    #dih
+        if "."
+    led.pixels_fill(BLACK)
+    led.pixels_show()
+    utime.sleep(0.2)
+    led.pixels_fill(WHITE)
+    led.pixels_show()
+    utime.sleep(0.2)   
+    led.pixels_fill(BLACK)
+    led.pixels_show()
+    utime.sleep(0.2)  
+
+if (morsecode == ".-")letter = 'a'
+   if (morsecode == "-...")letter = 'b'
+   if (morsecode == "-.-.")letter = 'c'
+   if (morsecode == "-..")letter = 'd'
+   if (morsecode == ".")letter = 'e'
+   if (morsecode == "..-.")letter = 'f'
+   if (morsecode == "--.")letter = 'g'
+   if (morsecode == "....")letter = 'h'
+   if (morsecode == "..")letter = 'i'
+   if (morsecode == ".---")letter = 'j'
+   if (morsecode == "-.-")letter = 'k'
+   if (morsecode == ".-..")letter = 'l'
+   if (morsecode == "--")letter = 'm'
+   if (morsecode == "-.")letter = 'n'
+   if (morsecode == "---")letter = 'o'
+   if (morsecode == ".--.")letter = 'p'
+   if (morsecode == "--.-")letter = 'q'
+   if (morsecode == ".-.")letter = 'r'
+   if (morsecode == "...")letter = 's'
+   if (morsecode == "-")letter = 't'
+   if (morsecode == "..-")letter = 'u'
+   if (morsecode == "...-")letter = 'v'
+   if (morsecode == ".--")letter = 'w'
+   if (morsecode == "-..-")letter = 'x'
+   if (morsecode == "-.--")letter = 'y'
+   if (morsecode == "--..")letter = 'z'
+   if (morsecode == ".----")letter = '1'
+   if (morsecode == "..---")letter = '2'
+   if (morsecode == "...--")letter = '3'
+   if (morsecode == "....-")letter = '4'
+   if (morsecode == ".....")letter = '5'
+   if (morsecode == "-....")letter = '6'
+   if (morsecode == "--...")letter = '7'
+   if (morsecode == "---..")letter = '8'
+   if (morsecode == "----.")letter = '9'
+   if (morsecode == "-----")letter = '10'
+   
+    /*--- Assign Values based on Random Letter---*/
+   if (letter == 'a') morsecode = ".-"
+   if (letter == 'b') morsecode = "-..."
+   if (letter == 'c') morsecode = "-.-."
+   if (letter == 'd') morsecode = "-.."
+   if (letter == 'e') morsecode = "."
+   if (letter == 'f') morsecode = "..-."
+   if (letter == 'g') morsecode = "--."
+   if (letter == 'h') morsecode = "...."
+   if (letter == 'i') morsecode = ".."
+   if (letter == 'j') morsecode = ".---"
+   if (letter == 'k') morsecode = "-.-"
+   if (letter == 'l') morsecode = ".-.."
+   if (letter == 'm') morsecode = "--"
+   if (letter == 'n') morsecode = "-."
+   if (letter == 'o') morsecode = "---"
+   if (letter == 'p') morsecode = ".--."
+   if (letter == 'q') morsecode = "--.-"
+   if (letter == 'r') morsecode = ".-."
+   if (letter == 's') morsecode = "..."
+   if (letter == 't') morsecode = "-"
+   if (letter == 'u') morsecode = "..-"
+   if (letter == 'v') morsecode = "...-"
+   if (letter == 'w') morsecode = ".--"
+   if (letter == 'x') morsecode = "-..-"
+   if (letter == 'y') morsecode = "-.--"
+   if (letter == 'z') morsecode = "--.."
+
